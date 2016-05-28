bb371f873230c2bacb21e3c9935d385dcaaba8b5
Reuben Brandt <Reubennbrandt@gmail.com>
2016-05-28 11:30:46 -0700

    Add Early routes and views

---
+++ app/controllers/query_controller.rb
@@ -0,0 +1,4 @@
+class QueryController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+   resources :query
    root 'initial#index'
 
   # Example of regular route:
