c02ae1ba1e6406878a3032d6f5d5401062445cd4
Reuben Brandt <Reubennbrandt@gmail.com>
2016-06-21 13:23:45 -0700

    Populates buttons with several values

--- app/controllers/initial_controller.rb
+++ app/controllers/initial_controller.rb
@@ -5,12 +5,8 @@ class InitialController < ApplicationController
     url = 'https://sf-survivor-services-api.herokuapp.com/organizations/'
     response = HTTParty.get(url)
     @organizations = response.parsed_response
-    @test = @organizations
-    p @test
-    # p @test["languages"]
     @organizations.each do |organization|
       language_list << organization["languages"]
-      p language_list
     end
     language_list.each do |language|
       language.each do |words|
