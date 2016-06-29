94a07068546d42a41e39c3b4bfb26fb1c4c12e04
Reuben Brandt <Reubennbrandt@gmail.com>
2016-06-29 13:28:53 -0700

    Improve controller

---
+++ app/controllers/queries_controller.rb
@@ -0,0 +1,16 @@
+class QueriesController < ApplicationController
+  include ApplicationHelper
+  def index
+    url = 'https://sf-survivor-services-api.herokuapp.com/organizations/'
+    response = HTTParty.get(url)
+    @organizations = response.parsed_response
+    language_lib(@organizations)
+    gender_lib(@organizations)
+    p "*****************#{@genders}******"
+  end
+
+  def show
+
+  end
+end
+
